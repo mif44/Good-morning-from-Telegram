ad60d214ffd4a23c8b9cb8e63ea888cf7fca8067
Denis <denis.shilyaev49@gmail.com>
2026-02-11 23:28:32 +0300

    text output correction

--- src/sending_message_telegram.py
+++ src/sending_message_telegram.py
@@ -17,7 +17,7 @@ async def data_display(bot):
         await quote_client_ru(),
     ]
 
-    text = "\n---------------------------------------------------------------------------------------\n".join(parts)
+    text = "\n----------------------------------------------------------------------------------------\n".join(parts)
     good_morning = sends_good_morning()
     await bot.send_message(chat_id, f"<b>{good_morning}</b>", parse_mode=ParseMode.HTML)
     await bot.send_message(chat_id, f"<b>{text}</b>", parse_mode=ParseMode.HTML)
